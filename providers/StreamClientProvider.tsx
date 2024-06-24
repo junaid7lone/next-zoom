a0817bf9df76ece42a27a63ee00690b6bd6e04c9
junaid7lone <junaid7lone@gmail.com>
2024-06-24 22:46:25 +0530

    fix stream

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,63 +1,37 @@
 "use client";
 
-import { tokenProvider } from "@/actions/stream.action";
-import Loader from "@/components/Loader";
-import { useUser } from "@clerk/nextjs";
-import {
-  StreamCall,
-  StreamVideo,
-  StreamVideoClient,
-  User,
-} from "@stream-io/video-react-sdk";
 import { ReactNode, useEffect, useState } from "react";
+import { StreamVideoClient, StreamVideo } from "@stream-io/video-react-sdk";
+import { useUser } from "@clerk/nextjs";
+import Loader from "@/components/Loader";
+import { tokenProvider } from "@/actions/stream.action";
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-// const client = new StreamVideoClient({ apiKey, user, token });
-// const call = client.call('default', 'my-first-call');
-// call.join({ create: true });
-
-export const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
   const [videoClient, setVideoClient] = useState<StreamVideoClient>();
   const { user, isLoaded } = useUser();
 
   useEffect(() => {
-    if (!isLoaded) {
-      return;
-    }
-    if (!apiKey) {
-      throw new Error("Steam API Key Missing!");
-    }
-
-    if (!user?.id) {
-      throw new Error("User Missing!");
-    }
+    if (!isLoaded || !user) return;
+    if (!API_KEY) throw new Error("Stream API key is missing");
 
     const client = new StreamVideoClient({
-      apiKey,
+      apiKey: API_KEY,
       user: {
         id: user?.id,
         name: user?.username || user?.id,
         image: user?.imageUrl,
       },
-      token: tokenProvider,
+      tokenProvider,
     });
 
     setVideoClient(client);
   }, [user, isLoaded]);
 
-  if (!videoClient) {
-    return <Loader />;
-  }
+  if (!videoClient) return <Loader />;
 
-  return (
-    <StreamVideo client={videoClient}>
-      {/* <StreamCall call={call}>
-        <MyVideoUI /> 
-      </StreamCall> */}
-      {children}
-    </StreamVideo>
-  );
+  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 
 export default StreamVideoProvider;
